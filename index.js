d4ade7f2d8271d5c7c30ff75378a17f5d68d0cab
Rich Trott <rtrott@gmail.com>
2017-03-08 23:20:21 -0800

    improve names

--- index.js
+++ index.js
@@ -2,19 +2,12 @@
 const metal = require('metal-name');
 const cheese = require('cheese-name');
 
-const first = (text) => {
-  return text.split(' ')[0];
-};
-
 const last = (text) => {
   return text.split(' ').pop();
 };
 
 module.exports = function () {
-  let cheeseName = cheese();
-  let metalName = metal();
-
   if (Math.random() < 0.5)
-	 return `${first(cheeseName)} ${last(metalName)}`;
-  return `${first(metalName)} ${last(cheeseName)}`;
+	 return `${metal({fragment: 'start'})} ${cheese()}`;
+  return `${cheese()} ${metal({fragment: 'end'})}`;
 };
