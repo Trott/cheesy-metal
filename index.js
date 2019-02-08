f9e3ddda7ca9192e2e4cc3cc45d484a15bae68fe
Rich Trott <rtrott@gmail.com>
2019-02-07 20:57:32 -0800

    remove unused code

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 const metal = require('metal-name');
 const cheese = require('cheese-name');
 
-const last = (text) => {
-  return text.split(' ').pop();
-};
-
 module.exports = function () {
   if (Math.random() < 0.5)
 	 return `${metal({fragment: 'start'})} ${cheese()}`;
