8af8993d1fae467cfc50e1dd7cb6a52e625e9843
Rich Trott <rtrott@gmail.com>
2020-10-17 19:57:21 -0700

    chore: enable standard linting

--- cli.js
+++ cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-'use strict';
-var meow = require('meow');
-var cheese = require('./');
+'use strict'
+var meow = require('meow')
+var cheese = require('./')
 
 meow({
-	help: [
-		'Examples',
-		'  $ cheesy-metal',
-		'  Queso Cult'
-	].join('\n')
-});
+  help: [
+    'Examples',
+    '  $ cheesy-metal',
+    '  Queso Cult'
+  ].join('\n')
+})
 
-console.log(cheese());
+console.log(cheese())
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-'use strict';
-const metal = require('metal-name');
-const cheese = require('cheese-name');
+'use strict'
+const metal = require('metal-name')
+const cheese = require('cheese-name')
 
 module.exports = function () {
-  if (Math.random() < 0.5)
-	 return `${metal({fragment: 'start'})} ${cheese()}`;
-  return `${cheese()} ${metal({fragment: 'end'})}`;
-};
+  if (Math.random() < 0.5) { return `${metal({ fragment: 'start' })} ${cheese()}` }
+  return `${cheese()} ${metal({ fragment: 'end' })}`
+}
