d66024be78934cccd770b176e3d7bc315a2878bf
Rich Trott <rtrott@gmail.com>
2020-10-17 19:58:58 -0700

    chore: enable standard linting (#6)

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-'use strict';
-var test = require('ava');
-var cheese = require('./');
+'use strict'
+var test = require('ava')
+var cheese = require('./')
 
 test('returns a random name', function (t) {
-	t.assert(cheese().length > 0);
-});
+  t.assert(cheese().length > 0)
+})
 
 test('returns a two-part name', function (t) {
-	t.assert(cheese().indexOf(' ') !== -1);
-});
+  t.assert(cheese().indexOf(' ') !== -1)
+})
 
 test('returns a different name each call', function (t) {
-	// Make sure there's variety.
-	const names = [cheese(), cheese(), cheese(), cheese(), cheese(), cheese()]
-	const unique = names.filter((val, idx, arr) => arr.indexOf(val) === idx);
-	t.assert(unique.length > 3);
+  // Make sure there's variety.
+  const names = [cheese(), cheese(), cheese(), cheese(), cheese(), cheese()]
+  const unique = names.filter((val, idx, arr) => arr.indexOf(val) === idx)
+  t.assert(unique.length > 3)
 })
